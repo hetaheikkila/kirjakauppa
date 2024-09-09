79a76605627f865cb8b50cb95a32bdadff53aab4
Heta <hhmj28@gmail.com>
2024-09-09 21:28:51 +0300

    kirjakauppa pohja

---
+++ kirjakauppa24/src/test/java/kirjak/kirjakauppa24/Kirjakauppa24ApplicationTests.java
@@ -0,0 +1,13 @@
+package kirjak.kirjakauppa24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kirjakauppa24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
